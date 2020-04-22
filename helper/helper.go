dabb49ad3ac39c7a13500c8b12e525916a5e9482
SteZ <stephenzhavrid@gmail.com>
2020-04-22 12:09:49 +0300

    add reuseble pkg and import it using go module onto main.go

---
+++ helper/helper.go
@@ -0,0 +1,15 @@
+/*
+* Author: Stefan
+* Craete: 04.22.2020 12:00
+* Last changes:04.22.2020 12:01
+* Task: Class Work Lec2
+ */
+
+package helper
+
+import "fmt"
+
+//Help function
+func Help() {
+	fmt.Println("Helper func!")
+}
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 /*
 * Author: Stefan
 * Craete: 04.22.2020 11:25
-* Last changes:04.22.2020 11:37
-* Task: Class Work Lec2
+* Last changes:04.22.2020 12:08
+* Task: Class Work Lec1-2
  */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"module/helper"
+)
 
 func main() {
 	fmt.Println("Hello golang world!!!")
+	helper.Help()
 }
