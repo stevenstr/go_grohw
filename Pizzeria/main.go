f8a8f2d48a91dc789f59006d42b74476910d4ec3
SteZ <stephenzhavrid@gmail.com>
2020-04-27 17:51:44 +0300

    Some changes

--- Pizzeria/main.go
+++ Pizzeria/main.go
@@ -1,7 +1,7 @@
 /*
 * Authod: Stefan
 * Created: 04.27.2020
-* Last changes: 04.27.2020 17:00
+* Last changes: 04.27.2020 18:59
 * Task: Class Work Lec3
  */
 
@@ -12,5 +12,25 @@ import (
 )
 
 func main() {
-	fmt.Println("HW Lec3")
+	var pizza string = "Marharita"
+
+	fmt.Println("Value: " + pizza)
+	fmt.Println(pizza)
+	fmt.Printf("value: %v | type: %T \n\r\n", pizza, pizza)
+
+	var pizza1 string
+	pizza1 = "Peperoni"
+
+	fmt.Println("Value: " + pizza1)
+	fmt.Println(pizza1)
+	fmt.Printf("value: %v | type: %T \n\r\n", pizza1, pizza1)
+
+	pizza2 := "Mashed"
+	fmt.Println("Value: " + pizza2)
+	fmt.Println(pizza2)
+	fmt.Printf("value: %v | type: %T \n\r\n", pizza2, pizza2)
+}
+
+func init() {
+	fmt.Printf("Project: Pizzeria\nVersion: 0.1\n\r\n")
 }
