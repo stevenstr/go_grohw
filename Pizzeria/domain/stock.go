ce304d170a7bf51252f7ae98d748b7752dcca564
SteZ <stephenzhavrid@gmail.com>
2020-05-04 21:14:12 +0300

    change reciever name

--- Pizzeria/domain/stock.go
+++ Pizzeria/domain/stock.go
@@ -4,7 +4,6 @@
 * Last changes: 05.04.2020 20:50
 * Task: Class Work Lec4 Project
 * This package for stock types
-*
  */
 
 package domain
@@ -15,9 +14,9 @@ import "fmt"
 type Stock []string
 
 //Print method - get copy of variable with Stock type, and can use her value called name stock
-func (stock Stock) Print() {
+func (s Stock) Print() {
 	fmt.Println("List of pizzas...")
-	for index, pizza := range stock {
+	for index, pizza := range s {
 		fmt.Println("id: ", index, " | pizza: ", pizza)
 	}
 }
